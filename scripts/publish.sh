c7cea120872771c959ed3546b71bc8c5788c92be
Andrew Kirchmyer <akirchmyer@gmail.com>
2022-08-04 16:08:15 -0400

    Removed unused files

--- scripts/publish.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-PUSH_TOKEN="TOKEN GOES HERE"
-echo "Setting auth token: $PUSH_TOKEN"
-npm set registry REGISTRY_URL
-npm set //REGISTRY_DOMAIN/:_authToken $PUSH_TOKEN
-npm publish \
-  && echo "Publish Complete!" \
-  || echo "Publish Failed - see error messages for more info!"
