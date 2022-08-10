5bbb445397e541352495be40dce9372db754cbcb
Andrew Kirchmyer <akirchmyer@gmail.com>
2022-08-10 12:08:18 -0400

    Added more options for config

--- src/index.test.ts
+++ src/index.test.ts
@@ -6,26 +6,38 @@ import { Template } from 'aws-cdk-lib/assertions';
 test('test default setup', () => {
     const app = new cdk.App();
     const userDataPath = path.join(__dirname, 'testUserData.sh');
-
     const stack = new Ec2CdkStack(app, 'StackIdentifier', {}, {
         userDataPath
     });
-
     const template = Template.fromStack(stack);
+    expect(template.toJSON()).toMatchSnapshot();
+});
 
+test('test with no user data', () => {
+    const app = new cdk.App();
+    const stack = new Ec2CdkStack(app, 'StackIdentifier', {}, {});
+    const template = Template.fromStack(stack);
     expect(template.toJSON()).toMatchSnapshot();
 });
 
 test('test with options', () => {
     const app = new cdk.App();
     const userDataPath = path.join(__dirname, 'testUserData.sh');
-
     const stack = new Ec2CdkStack(app, 'StackIdentifier', {}, {
         userDataPath,
-        image: 'image-id'
-    });
-
+        vpcProps: {
+            natGateways: 1,
+        },
+        securityGroupProps: {
+            allowAllOutbound: false
+        },
+        allowPorts: [8080],
+        image: 'image-id',
+        ec2Props: {
+            instanceName: 'MyInstance'
+        }
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } as any);
     const template = Template.fromStack(stack);
-
     expect(template.toJSON()).toMatchSnapshot();
 });
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
 import * as ec2 from "aws-cdk-lib/aws-ec2";
 import * as cdk from 'aws-cdk-lib';
 import * as iam from 'aws-cdk-lib/aws-iam'
-import { KeyPair } from 'cdk-ec2-key-pair';
+import * as KeyPair from 'cdk-ec2-key-pair';
 import { Asset } from 'aws-cdk-lib/aws-s3-assets';
 import { Construct } from 'constructs';
 
 interface Ec2CdkStackOptions {
-    userDataPath: string,
-    image?: string
+    userDataPath?: string,
+    vpcProps?: ec2.VpcProps,
+    securityGroupProps?: ec2.SecurityGroupProps,
+    allowPorts?: Array<number>,
+    image?: string,
+    ec2Props?: ec2.SecurityGroupProps
 }
 
 export class Ec2CdkStack extends cdk.Stack {
@@ -15,61 +19,67 @@ export class Ec2CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: cdk.StackProps, options: Ec2CdkStackOptions) {
     super(scope, id, props);
 
-    const { userDataPath, image } = options;
-
-    const key = new KeyPair(this, 'KeyPair', {
-       name: 'cdk-keypair',
-       description: 'Key Pair created with CDK Deployment',
+    const key = new KeyPair.KeyPair(this, `${id}KeyPair`, {
+       name: `${id}-keypair`,
+       description: 'Key Pair created with CDK Deployment'
     });
 
-    const vpc = new ec2.Vpc(this, 'VPC', {
+    const vpc = new ec2.Vpc(this, `${id}VPC`, {
       natGateways: 0,
       subnetConfiguration: [{
         cidrMask: 24,
         name: "asterisk",
         subnetType: ec2.SubnetType.PUBLIC
-      }]
+      }],
+      ...(options.vpcProps || {})
     });
 
-    const securityGroup = new ec2.SecurityGroup(this, 'SecurityGroup', {
+    const securityGroup = new ec2.SecurityGroup(this, `${id}SecurityGroup`, {
       vpc,
       description: 'Allow SSH (TCP port 22) in',
-      allowAllOutbound: true
+      allowAllOutbound: true,
+      ...(options.securityGroupProps || {})
     });
-    securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(22), 'Allow SSH Access')
-    securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(80), 'Allow HTTP')
 
-    const role = new iam.Role(this, 'ec2Role', {
-      assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com')
+    (options.allowPorts || [22, 80, 443]).forEach((port) => {
+        securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(port), `Allow ${port}`)
+    });
+
+    const role = new iam.Role(this, `${id}Ec2Role`, {
+      assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com'),
     })
     role.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonSSMManagedInstanceCore'))
 
     const ami = new ec2.GenericLinuxImage({
         // Amazon Linux 2 Kernel 5.10 AMI 2.0.20220719.0 x86_64 HVM gp2
-        'us-east-1': image || 'ami-090fa75af13c156b4',
+        'us-east-1': options.image || 'ami-090fa75af13c156b4',
     });
 
-    const ec2Instance = new ec2.Instance(this, 'Instance', {
+    const ec2Instance = new ec2.Instance(this, `${id}Instance`, {
       vpc,
       instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
       machineImage: ami,
       securityGroup: securityGroup,
       keyName: key.keyPairName,
-      role: role
+      role: role,
+      instanceName: `${id}Ec2Instance`,
+      ...(options.ec2Props || {})
     });
 
-    // Create an asset that will be used as part of User Data to run on first load
-    const asset = new Asset(this, 'Asset', { path: userDataPath });
-    const localPath = ec2Instance.userData.addS3DownloadCommand({
-      bucket: asset.bucket,
-      bucketKey: asset.s3ObjectKey,
-    });
+    if (options.userDataPath) {
+      // Create an asset that will be used as part of User Data to run on first load
+      const asset = new Asset(this, `${id}Asset`, { path: options.userDataPath });
+      const localPath = ec2Instance.userData.addS3DownloadCommand({
+        bucket: asset.bucket,
+        bucketKey: asset.s3ObjectKey,
+      });
 
-    ec2Instance.userData.addExecuteFileCommand({
-      filePath: localPath,
-      arguments: '--verbose -y'
-    });
-    asset.grantRead(ec2Instance.role);
+      ec2Instance.userData.addExecuteFileCommand({
+        filePath: localPath,
+        arguments: '--verbose -y'
+      });
+      asset.grantRead(ec2Instance.role);
+    }
 
     // Create outputs for connecting
     new cdk.CfnOutput(this, 'IP Address', { value: ec2Instance.instancePublicIp });
