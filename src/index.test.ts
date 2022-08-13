520fd4826d6347e4a8f042e633a8c2e937d2dd6e
Andrew Kirchmyer <akirchmyer@gmail.com>
2022-08-13 12:32:26 -0400

    added prettier

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,43 +1,48 @@
-import * as path from 'path';
-import * as cdk from 'aws-cdk-lib';
-import { Ec2CdkStack } from './index';
-import { Template } from 'aws-cdk-lib/assertions';
+import * as path from "path";
+import * as cdk from "aws-cdk-lib";
+import { Ec2CdkStack } from "./index";
+import { Template } from "aws-cdk-lib/assertions";
 
-test('test default setup', () => {
-    const app = new cdk.App();
-    const userDataPath = path.join(__dirname, 'testUserData.sh');
-    const stack = new Ec2CdkStack(app, 'StackIdentifier', {}, {
-        userDataPath
-    });
-    const template = Template.fromStack(stack);
-    expect(template.toJSON()).toMatchSnapshot();
+test("test default setup", () => {
+  const app = new cdk.App();
+  const userDataPath = path.join(__dirname, "testUserData.sh");
+  const stack = new Ec2CdkStack(
+    app,
+    "StackIdentifier",
+    {},
+    {
+      userDataPath,
+    }
+  );
+  const template = Template.fromStack(stack);
+  expect(template.toJSON()).toMatchSnapshot();
 });
 
-test('test with no user data', () => {
-    const app = new cdk.App();
-    const stack = new Ec2CdkStack(app, 'StackIdentifier', {}, {});
-    const template = Template.fromStack(stack);
-    expect(template.toJSON()).toMatchSnapshot();
+test("test with no user data", () => {
+  const app = new cdk.App();
+  const stack = new Ec2CdkStack(app, "StackIdentifier", {}, {});
+  const template = Template.fromStack(stack);
+  expect(template.toJSON()).toMatchSnapshot();
 });
 
-test('test with options', () => {
-    const app = new cdk.App();
-    const userDataPath = path.join(__dirname, 'testUserData.sh');
-    const stack = new Ec2CdkStack(app, 'StackIdentifier', {}, {
-        userDataPath,
-        vpcProps: {
-            natGateways: 1,
-        },
-        securityGroupProps: {
-            allowAllOutbound: false
-        },
-        allowPorts: [8080],
-        image: 'image-id',
-        ec2Props: {
-            instanceName: 'MyInstance'
-        }
+test("test with options", () => {
+  const app = new cdk.App();
+  const userDataPath = path.join(__dirname, "testUserData.sh");
+  const stack = new Ec2CdkStack(app, "StackIdentifier", {}, {
+    userDataPath,
+    vpcProps: {
+      natGateways: 1,
+    },
+    securityGroupProps: {
+      allowAllOutbound: false,
+    },
+    allowPorts: [8080],
+    image: "image-id",
+    ec2Props: {
+      instanceName: "MyInstance",
+    },
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    } as any);
-    const template = Template.fromStack(stack);
-    expect(template.toJSON()).toMatchSnapshot();
+  } as any);
+  const template = Template.fromStack(stack);
+  expect(template.toJSON()).toMatchSnapshot();
 });
